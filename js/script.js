e0f5cdb223854abc35635e938c658f7f1888fd95
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-01-12 20:12:01 +0100

    Fixes problems + add switch case

--- js/script.js
+++ js/script.js
@@ -16,13 +16,6 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
     */
 
 // #1 - #2
-/*
-let userKm = parseInt(prompt('Quanti chilometri devi percorrere?').trim());
-let userAge = parseInt(prompt('Quanti anni hai?').trim());
- 
-console.log(`I chilometri da percorrere sono ${userKm}km`);
-console.log(`L'utente ha ${userAge} anni`);
-*/
 
 const buttonSend = document.getElementById('send');
 const buttonDelete = document.getElementById('delete');
@@ -36,27 +29,52 @@ const ticketCode = document.getElementById('ticket-code');
 let ticketCost = document.getElementById('ticket-cost');
 
 buttonSend.addEventListener('click', function () {
-    const fullNameValue = fullName.value;
-    console.log(fullNameValue);
+    if (fullName.value !== '' && userKm.value !== '' && userAgeRange.value !== '') {
 
-    const userKmValue = userKm.value;
-    console.log(`${userKmValue}km`);
+        const fullNameValue = fullName.value;
+        console.log(fullNameValue);
 
-    const userAgeRangeValue = userAgeRange.value;
-    console.log(userAgeRangeValue);
+        const userKmValue = userKm.value;
+        console.log(`${userKmValue}km`);
 
-    let ticketName = document.getElementById('ticket-fullname');
-    ticketName.innerHTML = fullNameValue;
+        const userAgeRangeValue = userAgeRange.value;
+        console.log(userAgeRangeValue);
 
-    fullName.value = '';
-    userKm.value = '';
-    userAgeRange.value = '';
+        let ticketName = document.getElementById('ticket-fullname');
+        ticketName.innerHTML = fullNameValue;
 
-    const randomCarriage = Math.floor(Math.random() * 9) + 1;
-    ticketCarriage.innerText = randomCarriage;
+        const randomCarriage = Math.floor(Math.random() * 9) + 1;
+        ticketCarriage.innerText = randomCarriage;
 
-    const randomCode = Math.floor(Math.random() * 9999) + 1;
-    ticketCode.innerText = randomCode;
+        const randomCode = Math.floor(Math.random() * 9999) + 1;
+        ticketCode.innerText = randomCode;
+
+        let costoTotale = userKm.value * 0.21;
+        let prezzoScontato = 0;
+        let tipoUtente = userAgeRange.value;
+
+        switch (tipoUtente) {
+            case 'underage':
+                prezzoScontato = .20;
+                costoTotale = (costoTotale - (costoTotale * prezzoScontato)).toFixed(2);
+                console.log(`Il prezzo del biglietto per i minorenni è di ${costoTotale}€`);
+                break;
+            case 'over65':
+                prezzoScontato = .40;
+                costoTotale = (costoTotale - (costoTotale * prezzoScontato)).toFixed(2);
+                console.log(`Il prezzo del biglietto per gli over 65 è di ${costoTotale}€`);
+                break;
+            case 'overage':
+                costoTotale = costoTotale.toFixed(2);
+                console.log(`Il prezzo del biglietto senza sconti è di ${costoTotale}€`);
+        }
+
+        ticketCost.innerHTML = costoTotale;
+
+        fullName.value = '';
+        userKm.value = '';
+        userAgeRange.value = '';
+    }
 });
 
 buttonDelete.addEventListener('click', function () {
@@ -64,30 +82,3 @@ buttonDelete.addEventListener('click', function () {
     userKm.value = '';
     userAgeRange.value = '';
 })
-
-let costoTotale = userKm * 0.21;
-
-const underage = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
-const overage = [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64];
-const over65 = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
-
-// #3 - #3.1
-if (!isNaN(userKm) && !isNaN(userAgeRange) || (fullName)) {
-
-
-    let prezzoScontato = 0;
-
-    if (0 < userAgeRange && userAgeRange < 18) {
-        prezzoScontato = .20;
-        costoTotale = (costoTotale - (costoTotale * prezzoScontato)).toFixed(2);
-        console.log(`Il prezzo del biglietto per i minorenni è di ${costoTotale}€`);
-    } else if (userAgeRange >= 65 && userAgeRange < 110) {
-        prezzoScontato = .40;
-        costoTotale = (costoTotale - (costoTotale * prezzoScontato)).toFixed(2);
-        console.log(`Il prezzo del biglietto per gli over 65 è di ${costoTotale}€`);
-    } else {
-        costoTotale = costoTotale.toFixed(2);
-        console.log(`Il prezzo del biglietto senza sconti è di ${costoTotale}€`);
-    }
-    ticketCost.innerText = costoTotale;
-}
